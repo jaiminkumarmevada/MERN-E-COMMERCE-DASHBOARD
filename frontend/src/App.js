64dae03ade002507412573395334633d7815b159
Jaimin Mevada <jaiminkumarmevada@gmail.com>
2024-08-29 13:05:15 -0800

    React Router Dom implementing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './Nav.jsx'
+import {BrowserRouter , Routes , Route } from 'react-router-dom';
+
+
 
 function App() {
   return (
     <div className="App">
-     
+      
+     <BrowserRouter>
+     <h1>E-dashboard</h1>
+     <Nav/>  
+
+
+     <Routes>
+
+      <Route path="/" element={<h1>Product</h1>}/>
+      </Routes>
+     </BrowserRouter>
     </div>
   );
 }
